50e4e82c9c58aec06059a7beadcf1408cf212287
Vpavel123 <varankin.pavel2@gmail.com>
2021-02-22 11:59:56 +0300

    login

--- signin.php
+++ signin.php
@@ -8,9 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style-login.css">
     <title>Вход</title>
 </head>
 <body>
-    
+    <div class="main-container">
+        <form action="" method="post"></form>
+        <input type="text" placeholder="login" name=""/>
+        <input type="password" placeholder="password" name=""/>
+    </div>
 </body>
 </html>
